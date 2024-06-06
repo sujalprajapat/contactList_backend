270362593c7a51952987c87df03f23a9d04aaca1
sujalprajapat <168108552+sujalprajapat@users.noreply.github.com>
2024-06-06 17:19:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors(
   {
-    origin:"http://localhost:3000",
+    origin:"https://contact-list-frontend-tan.vercel.app",
     methods:["POST","GET","PATCH","DELETE"],
     credentials : true,
   }
